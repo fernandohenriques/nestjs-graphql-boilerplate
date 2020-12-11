92de5eb563fd494540f4d7a3c0b1dda4bd631b8b
Guilherme Ananias <noghartt@outlook.com>
2020-12-11 15:46:06 -0300

    add ValidationPipe to bootstrap fn

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
@@ -6,7 +7,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
